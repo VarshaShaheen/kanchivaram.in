9da93d3738d3e4862241c6532225a2a8b52bfe82
VarshaShaheen <varshashaheen2003@gmail.com>
2023-12-10 11:53:01 +0530

    added type to product in price range

--- src/app/price-category/[price]/page.tsx
+++ src/app/price-category/[price]/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 import React from "react";
-import {fetchProductsByCategory, fetchProductsByPriceRange} from "../../../../firebase";
+import {fetchProductsByPriceRange} from "../../../../firebase";
 import Footer from "@/app/components/footer";
 import Image from 'next/image'
 import "@/app/utils/css/category.css";
+import Link from "next/link";
 
 type Product = {
     id: string;
@@ -25,17 +26,22 @@ const PricePage = async ({ params }: { params: { price: string } }) =>{
             <h1 className="text-5xl text-center capitalize font-ardilla mb-12 mt-5">Shop now</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
                 {products?.map((product) => (
+                    <Link href="/product/[code]" as={`/product/${product.code}`} key={product.id}>
                     <div key={product.id} className="rounded overflow-hidden bg-white">
                         <div className="relative w-full h-80">
-                            <img
+                            <Image
                                 className=" w-full h-full object-cover absolute"
                                 src={product.image[0].downloadURL}
                                 alt={product.name}
+                                width={500}
+                                height={500}
                             />
-                            <img
+                            <Image
                                 className="w-full h-full object-cover absolute hover:opacity-100 opacity-0 transition-opacity duration-200"
                                 src={product.image[1].downloadURL}
                                 alt={product.name}
+                                width={500}
+                                height={500}
                             />
                         </div>
                         <div className="py-3">
@@ -54,6 +60,7 @@ const PricePage = async ({ params }: { params: { price: string } }) =>{
                             </div>
                         </div>
                     </div>
+                    </Link>
                 ))}
             </div>
             <div className="mt-8" id="footer">
