7d6e5e18564bc7adf1f1dfd223b24439876c91f0
VarshaShaheen <varshashaheen2003@gmail.com>
2023-12-10 10:56:24 +0530

    slick added

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Footer = () => {
     return (
@@ -8,7 +9,13 @@ const Footer = () => {
                 <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
                     <div>
                         <Link href="/" passHref>
-                                <img className="h-20 flex justify-center sm:justify-start" src="/logo.png" alt="Company Logo"  />
+                            <Image
+                                className="h-20 flex justify-center sm:justify-start"
+                                src="/logo.png"
+                                alt="Company Logo"
+                                width={150} // Set your width
+                                height={200} // Set your height
+                            />
                         </Link>
                     </div>
                     <div className="mt-12 lg:mt-0">
@@ -17,38 +24,60 @@ const Footer = () => {
                         </h4>
                         <ul className="mt-2 space-y-1">
                             <li>
-                                <Link href="/about" passHref >
-                                    <div >
-                                        About Us
-                                    </div>
+                                <Link href="/about" passHref>
+                                    <div>About Us</div>
                                 </Link>
                             </li>
                             <li>
                                 <Link href="/contact-us" passHref>
-                                <div className="text-base text-gray-500 hover:text-gray-900">
-                                    Contact Us
-                                </div>
+                                    <div className="text-base text-gray-500 hover:text-gray-900">
+                                        Contact Us
+                                    </div>
                                 </Link>
                             </li>
                             <li className="flex mt-2">
                                 <div>
-                                <Link href="/">
-                                    <img src="/social-icons/insta.png" className="w-7 h-7 mr-2 grayscale hover:grayscale-0 transition duration-300" />
-                                </Link>
-                                </div>
-                                <div>
                                     <Link href="/">
-                                        <img src="/social-icons/fb.png" className="w-7 h-7 mr-2 grayscale hover:grayscale-0 transition duration-300" />
+                                        <Image
+                                            src="/social-icons/insta.png"
+                                            className="w-7 h-7 mr-2 grayscale hover:grayscale-0 transition duration-300"
+                                            alt="Instagram"
+                                            width={28}
+                                            height={28}
+                                        />
                                     </Link>
                                 </div>
                                 <div>
                                     <Link href="/">
-                                    <img src="/social-icons/twitter.png" className="w-7 h-7 mr-2 grayscale hover:grayscale-0 transition duration-300" />
+                                        <Image
+                                            src="/social-icons/fb.png"
+                                            className="w-7 h-7 mr-2 grayscale hover:grayscale-0 transition duration-300"
+                                            alt="Facebook"
+                                            width={28}
+                                            height={28}
+                                        />
                                     </Link>
                                 </div>
                                 <div>
                                     <Link href="/">
-                                    <img src="/social-icons/youtube.png" className="w-7 h-7 mr-2 grayscale hover:grayscale-0 transition duration-300" />
+                                        <Image
+                                            src="/social-icons/twitter.png"
+                                            className="w-7 h-7 mr-2 grayscale hover:grayscale-0 transition duration-300"
+                                            alt="Twitter"
+                                            width={28}
+                                            height={28}
+                                        />
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link href="/">
+                                        <Image
+                                            src="/social-icons/youtube.png"
+                                            className="w-7 h-7 mr-2 grayscale hover:grayscale-0 transition duration-300"
+                                            alt="YouTube"
+                                            width={28}
+                                            height={28}
+                                        />
                                     </Link>
                                 </div>
                             </li>
@@ -75,9 +104,9 @@ const Footer = () => {
                             </li>
                             <li>
                                 <Link href="/privacy-policy">
-                                <div className="text-base text-gray-500 hover:text-gray-900" >
-                                    Privacy Policy
-                                </div>
+                                    <div className="text-base text-gray-500 hover:text-gray-900">
+                                        Privacy Policy
+                                    </div>
                                 </Link>
                             </li>
                             <li>
@@ -89,9 +118,9 @@ const Footer = () => {
                             </li>
                             <li>
                                 <Link href="/return-policy">
-                                <div className="text-base text-gray-500 hover:text-gray-900" >
-                                    Return Policy
-                                </div>
+                                    <div className="text-base text-gray-500 hover:text-gray-900">
+                                        Return Policy
+                                    </div>
                                 </Link>
                             </li>
                         </ul>
--- src/app/components/single-category.tsx
+++ src/app/components/single-category.tsx
@@ -15,6 +15,21 @@ type categoryProduct = {
     mrp: number;
 };
 
+type product = {
+    categories: string;
+    code: string;
+    description: string;
+    fabric: string;
+    height: string;
+    image: { downloadURL: string }[];
+    mrp: number;
+    name: string;
+    weight: string;
+    color: string;
+};
+
+
+
 const CategoryGallery = ({ product }) => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
