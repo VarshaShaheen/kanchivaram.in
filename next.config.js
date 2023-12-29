91195848423084cfabae6aebde8928d2c9374489
VarshaShaheen <varshashaheen2003@gmail.com>
2023-12-29 11:29:02 +0530

    carousal changed

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['firebasestorage.googleapis.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'firebasestorage.googleapis.com',
+                pathname: '**',
+            },
+        ],
     },
 }
+
 
 // next.ccaronfig.js
 module.exports = nextConfig;
--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -1,36 +1,36 @@
-"use client";
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+"use client"
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
 const CarouselComponent = () => {
+    const [windowWidth, setWindowWidth] = useState(0);
+
+    useEffect(() => {
+        setWindowWidth(window.innerWidth);
+        const handleResize = () => setWindowWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
-        <div className="mx-auto ">
-            <Carousel
-                autoPlay
-                infiniteLoop
-                interval={3000} // Change slide every 3 seconds (adjust as needed)
-                showStatus={false}
-                showThumbs={false}
-            >
-                <div  >
-                    <img
-                        src="/carousel/car2.jpg"
-                        alt="Slide 1"
-                    />
-                </div>
-                <div >
-                    <img
-                        src="/carousel/car2.jpg"
-                        alt="Slide 2"
-                    />
-                </div>
-                <div >
-                    <img
-                        src="/carousel/car2.jpg"
-                        alt="Slide 3"
-                    />
-                </div>
-            </Carousel>
+        <div className="relative w-full h-56 md:h-[80vh] sm:h-[50vh] sm:mt-4">
+            {windowWidth > 768 ? (
+                <Image
+                    src="/carousel/carousel-desktop.jpg"
+                    alt="Desktop view"
+                    layout="fill"
+                    objectFit="cover"
+                />
+            ) : (
+                <Image
+                    src="/carousel/carousel-mobile.jpg"
+                    alt="Mobile view"
+                    layout="fill"
+                    objectFit="contain"
+                />
+            )}
         </div>
+
     );
 };
 
