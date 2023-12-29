60ecd132076f2f99610bd16d3ad8df7937251864
VarshaShaheen <varshashaheen2003@gmail.com>
2023-12-29 16:11:17 +0530

    category gallery changed

--- src/app/components/categories.tsx
+++ src/app/components/categories.tsx
@@ -1,43 +1,37 @@
-import Image from "next/image";
-import Link from "next/link";
-import { CATEGORIES } from "@/app/utils/constants";
+// components/CategoryGalleryItem.tsx
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { CATEGORIES } from '@/app/utils/constants';
 
-type GridItemProps = {
-    image: {
-        src: string;
-        alt: string;
-    };
+interface CategoryGalleryItemProps {
     categoryName: string;
+    imageUrl: string;
     categoryUrl: string;
-};
+}
 
-function GridItem({ image, categoryName, categoryUrl }: GridItemProps){
+const CategoryGalleryItem: React.FC<CategoryGalleryItemProps> = ({ categoryName, imageUrl, categoryUrl }) => {
     return (
         <div className="mb-2 relative">
-
-                    <Image
-                        className="w-100"
-                        src={image.src}
-                        alt={image.alt}
-                        width={400}
-                        height={400}
-                    />
-                    <p className="absolute bottom-0.5 left-0.5 right-0.5 text-white text-gallery text-center font-sans ">{categoryName}</p>
-                    <div className="absolute top-0 bottom-0 left-0 right-0 opacity-0 hover:opacity-75 bg-gray-900 flex items-center justify-center">
-                        <Link href={categoryUrl}>
-                        <button className="text-white font-bold py-2 px-4 rounded border-2 border-white">Buy Now</button>
-                        </Link>
-                    </div>
+            <Image className="object-cover" src={imageUrl} alt="Category Image" width={300} height={200} />
+            <p className="absolute bottom-0.5 left-0.5 right-0.5 text-white text-gallery text-center font-sans ">
+                {categoryName}
+            </p>
+            <div className="absolute top-0 bottom-0 left-0 right-0 opacity-0 hover:opacity-75 bg-gray-900 flex items-center justify-center">
+                <Link href={categoryUrl}>
+                    <button className="text-white font-bold py-2 px-4 rounded border-2 border-white">Buy Now</button>
+                </Link>
+            </div>
         </div>
     );
-}
+};
 
 const CategoryGallery = () => {
     return (
         <div className="gallery">
             <div className="grid m-0 grid-cols-1 grid-rows-1 lg:grid-cols-3 gap-2">
                 {CATEGORIES.map((category, i) => (
-                    <GridItem key={i} categoryName={category.name} image={category.image} categoryUrl={category.url} />
+                    <CategoryGalleryItem key={i} categoryName={category.name} imageUrl={category.image.src} categoryUrl={category.url} />
                 ))}
             </div>
         </div>
--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -13,7 +13,7 @@ export const CATEGORIES = [
     getCategory("Tissue Kanchipuram Silk", "/gallery/0Z7A6552.JPG", "/category/tissue-kanchipuram"),
     getCategory("Brocade", "/gallery/0Z7A6426.JPG", "/category/brocade"),
     getCategory("Organza Tissue", "/gallery/0Z7A6443.JPG", "/category/organza-tissue"),
-    getCategory("Pure Kanchipuram Silk", "/gallery/0Z7A6100 (1).JPG", "/category/pure-kanchipuram"),
+    getCategory("Pure Kanchipuram Silk", "/gallery/0Z7A6554.JPG", "/category/pure-kanchipuram"),
     getCategory("Madka Silk", "/gallery/0Z7A6550.JPG", "/"),
     getCategory("Pure Designer Kanchipuram Silk ", "/gallery/0Z7A6554.JPG", "/"),
 ]
